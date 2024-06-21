8d2b20efa506d357bfb3c71b908d0468e6263ee2
vivek <chimnanivivek14@gmail.com>
2024-06-21 12:10:18 +0530

    showing the snackbar message

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -13,18 +13,7 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-  final List<Expense> _registeredExpenses = [
-    Expense(
-        title: "Flutter Course",
-        amount: 19.99,
-        date: DateTime.now(),
-        category: Category.work),
-    Expense(
-        title: "Cinema",
-        amount: 15.99,
-        date: DateTime.now(),
-        category: Category.leisure),
-  ];
+  final List<Expense> _registeredExpenses = [];
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
@@ -43,15 +32,49 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Expense deleted!"),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(
+                expenseIndex,
+                expense,
+              );
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text(
+        "No expenses found. Try adding some!",
+      ),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Expense Tracker"),
         actions: [
           IconButton(
             onPressed: _openAddExpenseOverlay,
@@ -64,10 +87,9 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text("The Chart..."),
-          ExpensesList(
-            expenses: _registeredExpenses,
-            onRemoveExpense: _removeExpense,
-          )
+          Expanded(
+            child: mainContent,
+          ),
         ],
       ),
     );
